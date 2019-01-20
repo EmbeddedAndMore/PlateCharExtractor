ca46485a44b6d2ffde29b288b00af7f01d8d5c5f
M.Armoun <mohamad_armoon@yahoo.com>
2019-01-20 13:52:51 +0330

    initial commit

---
+++ PlateCharExtractor/Model/ThumbnailModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System.Collections.ObjectModel;
+using System.Windows.Media;
+
+namespace PlateCharExtractor.Model
+{
+    public class ThumbnailModel 
+    {
+        public string ThumbAddr { get; set; }
+        public ImageSource Thumbnail { get; set; }
+    }
+}
---
+++ PlateCharExtractor/ViewModel/MainViewModel.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using GalaSoft.MvvmLight;
+using PlateCharExtractor.Model;
+using ThumbnailSharp;
+
+namespace PlateCharExtractor.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        public List<ThumbnailModel> ThumbnaiList{ get; set; }
+        private ThumbnailModel _selectedThumbnail;
+
+        public ThumbnailModel SelectedThumbnail
+        {
+            get => _selectedThumbnail;
+            set
+            {
+                _selectedThumbnail = value;
+                OnOperatingImage = _selectedThumbnail.ThumbAddr;
+                RaisePropertyChanged(()=>OnOperatingImage);
+            }
+        }
+
+        public string OnOperatingImage { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            if (IsInDesignMode)
+            {
+            }
+            else
+            {
+                ThumbnaiList = new List<ThumbnailModel>();
+                ThumbnaiList.AddRange(GenerateThumbnails());
+
+
+            }
+        }
+
+
+
+        private List<ThumbnailModel> GenerateThumbnails()
+        {
+            List<string> imageAddrs = Directory.GetFiles(@"C:\Users\Mohamad\Desktop\thumbs", "*.jpg").ToList();
+            List<ThumbnailModel> images = new List<ThumbnailModel>();
+            foreach (var image in imageAddrs)
+            {
+                Stream resultStream = new ThumbnailCreator().CreateThumbnailStream(
+                    thumbnailSize: 200,
+                    imageFileLocation: image,
+                    imageFormat: Format.Jpeg
+                );
+                var imageSource = new BitmapImage();
+                    imageSource.BeginInit();
+                    imageSource.StreamSource = resultStream;
+                    imageSource.EndInit();
+                    ImageSource src = imageSource;
+                images.Add(new ThumbnailModel(){ThumbAddr = image, Thumbnail = imageSource});
+            }
+            return images;
+        }
+    }
+}
