a87177486f992a4610d05a005e774f4faae590fb
EmbeddedAndMore <mohamad_armoon@yahoo.com>
2019-01-21 23:50:52 +0330

    Add zoomableImage control

---
+++ PlateCharExtractor/Controls/ZoomableImageView.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace PlateLetterExtractor.Controls
+{
+    /// <summary>
+    /// Interaction logic for ZoomableImageView.xaml
+    /// </summary>
+    public partial class ZoomableImageView : UserControl
+    {
+
+
+        public ImageSource SourceImage
+        {
+            get { return (ImageSource)GetValue(SourceImageProperty); }
+            set { SetValue(SourceImageProperty, value); }
+        }
+        public static readonly DependencyProperty SourceImageProperty =
+            DependencyProperty.Register("sourceImage", typeof(ImageSource), typeof(ZoomableImageView), new FrameworkPropertyMetadata(
+            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
+        //public static ImageSource sourceImage
+        //{
+        //    get => MyImage.Source;
+        //    set
+        //    {
+        //        MyImage.Source = value;
+        //    }
+        //}
+        public ZoomableImageView()
+        {
+            InitializeComponent();
+        }
+
+
+        public void SetImage(ImageSource image)
+        {
+            MyImage.Source = image;
+        }
+
+        private void MyImage_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+
+        }
+
+        private void MyImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void MyImage_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
